ca62ed1bbf5cb285ecf0f13885d40940a2f2214a
jaychboy <116920645+jaychboy@users.noreply.github.com>
2024-06-27 12:11:32 -0400

    Update aliensettings.py

--- aliensettings.py
+++ aliensettings.py
@@ -10,7 +10,9 @@ class Settings:
         self.ship_speed = 3
 
         #Bullet
-        self.bullet_speed = 2.0
+        self.bullet_speed = 3.2
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = (60, 60, 60)
+        self.bullet_color = (25, 235, 0)
+
+        self.bullets_allowed = 8
